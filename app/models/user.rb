59d077ae40833ab56f1db039522bab661a422c06
akiyoshiaki <cpshunya@gmail.com>
2017-06-09 01:13:51 +0900

    6_40まで

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: {maximum: 50}
+  validates :email, presence: true, length: {maximum: 255},
+                format: {with: VALID_EMAIL_REGEX},
+                uniqueness: {case_sensitive: false}
+
+  # railsのメソッドでパスワード実装がほぼ完了
+  # モデル内にpassword_digestという属性が含まれていれば使える
+  has_secure_password
+  # セキュアにハッシュ化したパスワードを、データベース内のpassword_digestという属性に保存できるようになる。
+  # 2つのペアの仮想的な属性 (passwordとpassword_confirmation) が使えるようになる。また、存在性と値が一致するかどうかのバリデーションも追加される18 。
+  # authenticateメソッドが使えるようになる (引数の文字列がパスワードと一致するとUserオブジェクトを、間違っているとfalseを返すメソッド) 。
+
+end
